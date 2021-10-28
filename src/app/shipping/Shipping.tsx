443d37cc058e09b7548f6d0a422f9f738e9af40e
yarovojigor1988 <yarovojigo1988@gmail.com>
2021-10-28 10:57:00 +0300

    use library

--- src/app/shipping/Shipping.tsx
+++ src/app/shipping/Shipping.tsx
@@ -15,6 +15,7 @@ import { MultiShippingFormValues } from './MultiShippingForm';
 import ShippingForm from './ShippingForm';
 import ShippingHeader from './ShippingHeader';
 import { SingleShippingFormValues } from './SingleShippingForm';
+import { browserName } from 'react-device-detect';
 
 export interface ShippingProps {
     isBillingSameAsShipping: boolean;
@@ -164,30 +165,7 @@ class Shipping extends Component<ShippingProps & WithCheckoutShippingProps, Ship
             methodId,
         } = this.props;
 
-        let sBrowser, sUsrAg = navigator.userAgent;
-       
-        if (sUsrAg.indexOf("Firefox") > -1) {
-            sBrowser = "Mozilla Firefox";
-            //"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
-        } else if (sUsrAg.indexOf("OPR") > -1) {
-            sBrowser = "Opera";
-        } else if (sUsrAg.indexOf("Trident") > -1) {
-            sBrowser = "Microsoft Internet Explorer";
-            //"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko"
-        } else if (sUsrAg.indexOf("Edge") > -1) {
-            sBrowser = "Microsoft Edge";
-            //"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
-        } else if (sUsrAg.indexOf("Chrome") > -1) {
-            sBrowser = "Google Chrome or Chromium";
-            //"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
-        } else if (sUsrAg.indexOf("Safari") > -1) {
-            sBrowser = "Apple Safari";
-            //"Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
-        } else {
-            sBrowser = "unknown";
-        }
-
-        if (addressValues?.customFields.field_35) addressValues.customFields.field_35 = sBrowser;
+        if (addressValues?.customFields?.field_35) {addressValues.customFields.field_35 = browserName;}
 
         const updatedShippingAddress = addressValues && mapAddressFromFormValues(addressValues);
         const promises: Array<Promise<CheckoutSelectors>> = [];
